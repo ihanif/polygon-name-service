74c9e31768734875323400b0dbabdb826bdbbfd9
Hanif Rahman <hanifkhan@gmail.com>
2022-02-24 22:56:10 +0000

    feat: Add new domain minting test

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,21 +5,18 @@ const main = async () => {
 
   console.log("Contract deployed to:", domainContract.address);
 
-  let txn = await domainContract.register("rollups", {
+  let txn = await domainContract.register("web3", {
     value: hre.ethers.utils.parseEther("0.1"),
   });
   await txn.wait();
-  console.log("Minted domain rollups.stack");
+  console.log("Minted domain web3.stack");
 
-  txn = await domainContract.setRecord(
-    "rollups",
-    "What counts as a rollups stack?"
-  );
+  txn = await domainContract.setRecord("web3", "What counts as a web3 stack?");
   await txn.wait();
-  console.log("Set record for rollups.stack");
+  console.log("Set record for web3.stack");
 
-  const address = await domainContract.getAddress("rollups");
-  console.log("Owner of domain rollups:", address);
+  const address = await domainContract.getAddress("web3");
+  console.log("Owner of domain web3:", address);
 
   const balance = await hre.ethers.provider.getBalance(domainContract.address);
   console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
